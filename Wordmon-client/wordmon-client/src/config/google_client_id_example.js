a4cf03ea0f20e3ce98575e192d472228447a0ea2
Minkyung Lee <gooogyeong@gmail.com>
2020-04-13 19:03:31 +0900

    Chore: test gitignore

--- Wordmon-client/wordmon-client/src/config/google_client_id_example.js
+++
@@ -1,7 +0,0 @@
-//!반.드.시 git push할때는 client id 지워야합니다!
-//!Github에 push X
-//gitignore에는 /src/config/google_client_id.js이렇게 되어있기 때문에
-//해당 파일의 파일명이 google_client_id.js일때는 push되지 않습니다.
-//TODO: 이 파일의 이름에서 example을 삭제, google_client_id.js로 만든 후에
-//TODO: google client id를 입력
-export const GOOGLE_CLIENT_ID = YOUR_GOOGLE_CLIENT_ID;
