735e03f98c78faa90c67f83d6f35bb2a546cd2f7
Minkyung Lee <gooogyeong@gmail.com>
2020-04-13 18:55:53 +0900

    Chore: edited gitignore and config

--- Wordmon-client/wordmon-client/src/config/google_client_id.js
+++ Wordmon-client/wordmon-client/src/config/google_client_id.js
@@ -4,5 +4,4 @@
 //해당 파일의 파일명이 google_client_id.js일때는 push되지 않습니다.
 //TODO: 이 파일의 이름에서 example을 삭제, google_client_id.js로 만든 후에
 //TODO: google client id를 입력
-export const GOOGLE_CLIENT_ID =
-  "543940618752-ngublacb476a1q4or68knsajp0so6e37.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_ID = YOUR_GOOGLE_CLIENT_ID;
